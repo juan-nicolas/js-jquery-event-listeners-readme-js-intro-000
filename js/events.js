116f457f467ce9320fb52801892d77dcd1f9be94
Juan Nicolas <jnma91@gmail.com>
2018-10-13 19:55:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img')[0].classList.add('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(keypressed){
+    if(keypressed.key === 'G'){
+      alert('You entered the letter G')
+    }
+  })
+}
+
+function submitIt() {
+
+}
 
 $(document).ready(function(){
-
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
