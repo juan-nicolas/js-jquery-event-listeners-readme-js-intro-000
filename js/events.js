9ba25171d8ae7a85a8f5ab70f4f06495e931b567
Juan Nicolas <jnma91@gmail.com>
2018-10-13 19:08:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,18 +1,20 @@
 //define functions here
 function getIt() {
-  
+  $('p').on('click', function(){
+    alert('Hey!')
+  });
 }
 
 function frameIt() {
-  
+
 }
 
 function pressIt() {
-  
+
 }
 
 function submitIt() {
-  
+
 }
 
 $(document).ready(function(){
